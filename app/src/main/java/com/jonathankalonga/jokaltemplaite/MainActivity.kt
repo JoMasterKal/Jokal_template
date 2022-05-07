fd2435871227d78bc6d74f1899179415d8152e31
jonathan kalonga <jokalmwamba@gmail.com>
2022-05-07 08:57:38 +0200

    finish to fix the bacground image

--- app/src/main/java/com/jonathankalonga/jokaltemplaite/MainActivity.kt
+++ app/src/main/java/com/jonathankalonga/jokaltemplaite/MainActivity.kt
@@ -3,13 +3,22 @@ package com.jonathankalonga.jokaltemplaite
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.jonathankalonga.jokaltemplaite.models.Shop
+import com.jonathankalonga.jokaltemplaite.models.shoesProduct
+import com.jonathankalonga.jokaltemplaite.ui.AppBarCollapsedHeight
+import com.jonathankalonga.jokaltemplaite.ui.AppBarExpendedHeight
 import com.jonathankalonga.jokaltemplaite.ui.theme.JokalTemplaiteTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +31,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    MyApp(shoesProduct)
                 }
             }
         }
@@ -30,14 +39,43 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyApp(shop: Shop) {
+    Box() {
+        Content(shop)
+        ParalaxToolBar(shop)
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun ParalaxToolBar(shop: Shop) {
+    val imageHeight = AppBarExpendedHeight - AppBarCollapsedHeight
+    TopAppBar(
+        contentPadding = PaddingValues(),
+        backgroundColor = Color.White,
+        modifier = Modifier.height(imageHeight)
+    ) {
+        Column {
+            Box {
+                Image(painter = painterResource(id =R.drawable.shop ),
+                    contentDescription ="shop image",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+        }
+    }
+
+}
+
+@Composable
+fun Content(shop: Shop){
+
+}
+
+@Preview(showBackground = true, widthDp = 300, heightDp = 1400)
 @Composable
 fun DefaultPreview() {
     JokalTemplaiteTheme {
-        Greeting("Android")
+        MyApp(shoesProduct)
     }
 }
